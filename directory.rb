061fbe3832cff84640f8978418867e33af505820
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-07-26 08:32:48 +0100

    Refactor interactive_menu into separate methods

--- directory.rb
+++ directory.rb
@@ -1,29 +1,39 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" 
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      if students.length > 0
-        print_header
-        print_this(students)
-        print_footer(students)
-      end
-    when "9"
-      exit # this will cause program to terminate
-    else
-      puts "I don't know what you meant, try again"
+@students = [] # empty array accessible to all methods
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    # input the students
+    input_students
+  when "2"
+    # show the students
+    if @students.length > 0
+      show_students
     end
+  when "9"
+    exit # this will cause program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
@@ -31,8 +41,6 @@ def input_students
 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create empty array
-  students = []
 
   # get the first name
   name = gets.chomp
@@ -53,11 +61,11 @@ def input_students
   while !name.empty? do
 
     # add the student hash to the array 
-    students << {name: name, country_born: country_born, sport: sport, cohort: cohort.to_s}
-    if students.count == 1
+    @students << {name: name, country_born: country_born, sport: sport, cohort: cohort.to_s}
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
 
     # get another name from the user
@@ -68,18 +76,15 @@ def input_students
       break
     end
 
+    puts "What cohort?"
+    cohort = gets.chomp
+
     puts "What is their country of birth?"
     country_born = gets.chomp
 
     puts "What is their favourite sport?"
     sport = gets.chomp
-
-    puts "What cohort?"
-    cohort = gets.chomp
-
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -87,12 +92,12 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print_this(students)
+def print_students_list
   counter = 0
 
   students_by_cohort = {}
 
-  students.each do |student|
+  @students.each do |student|
     cohort = student[:cohort]
 
     if students_by_cohort[cohort] == nil
@@ -118,11 +123,11 @@ def print_this(students)
     # end
 end
 
-def print_footer(names)
-  if names.length == 1
+def print_footer
+  if @students.length == 1
     puts "Overall, we have 1 great student".center(50)
   else
-    puts "Overall, we have #{names.count} great students".center(50)
+    puts "Overall, we have #{@students.count} great students".center(50)
   end
 end
 
