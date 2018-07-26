6e5ad5906c08199d5d2847a29600145784c89098
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-07-26 09:20:54 +0100

    Add loading from csv file

--- directory.rb
+++ directory.rb
@@ -43,6 +43,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" 
 end
 
@@ -64,6 +65,8 @@ def process(selection)
     end
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause program to terminate
   else
@@ -77,8 +80,6 @@ def interactive_menu
     process(gets.chomp)
   end
 end
-
-
 
 def print_header
   puts "The students of Villains Academy".center(50)
@@ -108,7 +109,6 @@ def print_students_list
     end
     puts ""
 
-    
   end
     # while counter < students.length 
     #   puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)".center(50)
@@ -132,6 +132,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
